b7955b96ee87dc018e556bc930c10fef6f4cf06e
marta-ledwon <marta-ledwon@users.noreply.github.com>
2022-07-06 19:25:52 +0200

    próba

---
+++ src/main/java/pl/cyber/trainees/wyjaśnienia/Talk.java
@@ -0,0 +1,28 @@
+package pl.cyber.trainees.wyjaśnienia;
+
+import java.math.BigDecimal;
+
+public class Talk {   // tu zaczyna się klasa
+
+        private String spacja = " ";  // parametr klasy
+
+        public String nazwa_metody(String parametr) { // tu zaczyna się klasa nr 1
+            //Metoda coś wykonuje
+
+            return "zwracamy napis";
+        }// tu kończy się klasa nr 1
+
+        private BigDecimal nazwa_metody2(BigDecimal parametr) {// tu zaczyna się klasa nr 2
+            //Metoda coś wykonuje
+//    return BigDecimal.ONE;
+
+            String liczba1 = "1";
+            String liczba2 = "12";
+
+            return new BigDecimal(liczba1 + liczba2); // 112
+
+        }// tu kończy się klasa nr 1
+
+    } // tu kończy się nasza klasa
+
+
