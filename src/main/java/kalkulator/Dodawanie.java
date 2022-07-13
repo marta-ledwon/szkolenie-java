aff109c8862f94d7906759f620bda86a14b607f7
marta-ledwon <marta-ledwon@users.noreply.github.com>
2022-07-13 16:55:19 +0200

    sprawdzenie czy działa - druga praca domowa

---
+++ src/main/java/kalkulator/Dodawanie.java
@@ -0,0 +1,16 @@
+package kalkulator;
+
+import java.math.BigDecimal;
+
+public class Dodawanie {
+
+    public String name = "Ala";
+   public int age = 20;
+    public double cokolwiek = 1.5;
+    public BigDecimal cokolwiek2 = BigDecimal.valueOf(15.0);
+
+    }
+
+
+
+
---
+++ src/main/java/kalkulator/Dzielenie.java
@@ -0,0 +1,12 @@
+package kalkulator;
+
+import java.math.BigDecimal;
+
+public class Dzielenie {
+
+    public String name = "Iza";
+    public int age = 33;
+    public double cokolwiek = 6.7;
+    public BigDecimal cokolwiek2 = BigDecimal.valueOf(132.2);
+}
+
