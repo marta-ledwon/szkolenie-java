b53b111031c5f600fcb3233f86d404f0bd67eb16
marta-ledwon <marta-ledwon@users.noreply.github.com>
2022-07-22 20:06:05 +0200

    czyszczenie simpleApp

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -1,4 +1,16 @@
+//1. Konstrukcja klas i metod
 package pl.cyber.trainees;
+
+/* oreślenie pakietu wramach projektu, gdzie znajduje się nasza klasa.
+pole package ustawia się automatycznie jeśli nasz plik zawiera się w odpowiedniej strukturze
+katalogów tj src/main/java/......
+w naszym przykładzie src/main/java/pl.cyber.trainees */
+
+//region miejsce na importy
+import java.io.File;  //importy zazwyczaj dodają się same ewemtualnie można je dodać poprzez Ctrl + Enter
+//poprzez importy powinniśmy rozumieć użycie dodatkowych bibliotek w naszej klasie
+//endregion
+
 
 public class SimpleApp {
     public static void main(String[] args) {
