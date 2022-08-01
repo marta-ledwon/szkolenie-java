8e4c4971a8d1a260dee4d16467f78a6885367d6d
marta-ledwon <marta-ledwon@users.noreply.github.com>
2022-08-01 18:14:49 +0200

    rozwiązanie pracy domowej

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -4,12 +4,8 @@ import pl.cyber.trainees.wyjaśnienia.Petle;
 
 public class SimpleApp {
     public static void main(String[] args) {
-        Petle petle = new Petle();
-        petle.uruchom();
 
-//    InstrukcjeWarunkowe instrukcjeWarunkowe = new InstrukcjeWarunkowe();
-//    instrukcjeWarunkowe.uruchom();
-
-
+    Petle petle = new Petle();
+    petle.uruchom();
     }
 }
